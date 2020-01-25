0432720ba77e3f0ef78cfb8500a2784d5762c0c8
dazocc <dazocc@gmail.com>
2020-01-25 15:17:16 -0400

    finalizado

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const routes = require('./src/routes');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 //inicia o ap
 const app = express();
 
+//aceitar json
+app.use(express.json());
+//aceitar requisicao de outros dominios
+app.use(cors());
+
 //iniciando o DB
 mongoose.connect('mongodb://localhost:27017/backendph', { 
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 //leitura de todos os models para o mongoose
 requireDir('./src/models');
 
 //rotas
-app.use('/api', require('./src/routes'));
+app.use('/api', routes);
 
 app.listen(3001);
